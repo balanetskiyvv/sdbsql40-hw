20c4b453a5e8978e8833df25de048eba998bc70f
Vasiliy Balanetskiy <33crackman@gmail.com>
2025-04-12 02:22:10 +0700

    Add files via upload

---
+++ sdsqlHW7/vertical/conf/postgres_users/insert_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (user_id, name, email, address, phone_number)
+VALUES (1, Julio, 'julio@test.su', 'Norilsk', 999-4523),
+       (2, Vasyan, 'vasyan@test.su', 'Norilsk', 999-3333),
+       (3, Katya, 'katya@test.su', 'Orel', 888-1422),
+       (4, Evgeniy 'evgeniy@test.su', 'Norilsk', 999-5783),
+       (5, Maria, 'maria@test.su', 'Moscow', 444-0765),
+       (6, Petr, 'petr@test.su', 'Voronezh', 111-4592),
+       (7, Roman, 'Roman@test.su', 'Moscow', 444-4523),
+       (8, Alex, 'alex@test.su', 'Saint-Petersburg', 555-7502),
+       (9, Polina, 'polina@test.su', 'Voronezh', 111-1234),
+       (10, Julio, 'julioTako@test.su', 'Saint-Petersburg', 555-7654);
---
+++ sdsqlHW7/vertical/conf/postgres_users1/shards.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users_common
+(
+    user_id int not null,
+    name character varying not null,
+    email character varying not null
+);
