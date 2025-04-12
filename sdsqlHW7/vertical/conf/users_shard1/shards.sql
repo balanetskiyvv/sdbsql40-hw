3653100166529d115bd5c3636678b062bf2193b1
Vasiliy Balanetskiy <33crackman@gmail.com>
2025-04-12 12:33:52 +0300

    Delete sdsqlHW7 directory
    reupload2

--- sdsqlHW7/vertical/conf/users_shard1/shards.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE shard_database_1;
-
-CREATE TABLE users_shard1
-(
-    user_id int PRIMARY KEY,
-    username character varying not null
-);
--- sdsqlHW7/vertical/conf/users_shard2/shards.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE shard_database_2;
-
-CREATE TABLE users_shard2
-(
-    user_id int PRIMARY KEY,
-    password character varying not null
-);
