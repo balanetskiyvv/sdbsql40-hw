b790c09888133a794ac98c40f53eaad561446b44
Vasiliy Balanetskiy <33crackman@gmail.com>
2025-04-12 11:51:21 +0300

    Delete sdsqlHW7 directory
    reupload

--- sdsqlHW7/vertical/conf/postgres_users1/shards.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users_common
-(
-    user_id int not null,
-    name character varying not null,
-    email character varying not null
-);
--- sdsqlHW7/vertical/conf/postgres_users2/shards.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users_details
-(
-    user_id int not null,
-    address character varying not null,
-    phone_number character varying not null,
-    CONSTRAINT pk_users_details primary key (user_id)
-);
